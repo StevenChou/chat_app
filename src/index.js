7144d7184cb722a0c3d7b89a2e5da9e47797ae68
whalebro <steven.chou.second@gmail.com>
2020-09-03 16:36:04 +0800

    Socket.io Events

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+// 包裝了 http server
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+let count = 0;
+
+io.on('connection', (socket) => {
+  console.log('New WebSocket connection');
+
+  socket.emit('countUpdated', count);
+
+  socket.on('increment', () => {
+    count++;
+    // to single connection
+    // socket.emit('countUpdated', count);
+
+    // to all connections
+    io.emit('countUpdated', count);
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
 });

11f68ed364d381a39773da0d4b1245b512e2dc04
whalebro <steven.chou.second@gmail.com>
2020-09-04 14:50:44 +0800

    Socket.io Rooms

--- src/index.js
+++ src/index.js
@@ -20,9 +20,18 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', (socket) => {
   console.log('New WebSocket connection');
 
-  socket.emit('message', generateMessage('Welcome!'));
-  // 除了自己以外的其他 connections
-  socket.broadcast.emit('message', generateMessage('A new user has joined!'));
+  socket.on('join', ({ username, room }) => {
+    socket.join(room);
+
+    socket.emit('message', generateMessage('Welcome!'));
+    // 除了自己以外的其他 connections
+    socket.broadcast
+      .to(room)
+      .emit('message', generateMessage(`${username} has joined!`));
+
+    // socket.emit, io.emit, socket.broadcast.emit
+    // io.to.emit, socket.broadcast.to.emit
+  });
 
   socket.on('sendMessage', (message, callback) => {
     const filter = new Filter();
@@ -35,7 +44,7 @@ io.on('connection', (socket) => {
     // socket.emit('message', message);
 
     // to all connections
-    io.emit('message', generateMessage(message));
+    io.to('Center City').emit('message', generateMessage(message));
     callback();
   });
 

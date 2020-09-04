c3b1d95c16f95e1630c3bff66370b0bed329e408
whalebro <steven.chou.second@gmail.com>
2020-09-04 16:45:08 +0800

    Rendering User List

--- src/index.js
+++ src/index.js
@@ -43,6 +43,10 @@ io.on('connection', (socket) => {
         'message',
         generateMessage('Admin', `${user.username} has joined!`)
       );
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
 
     callback();
     // socket.emit, io.emit, socket.broadcast.emit
@@ -85,6 +89,10 @@ io.on('connection', (socket) => {
         'message',
         generateMessage('Admin', `${user.username} has left!`)
       );
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
     }
   });
 });
